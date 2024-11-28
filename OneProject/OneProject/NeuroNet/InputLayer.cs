827a9835aa5a265868384e32810a745c4b6a6de5
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2024-11-28 15:31:48 +0300

    Add files via upload

---
+++ OneProject/OneProject/NeuroNet/InputLayer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+
+namespace OneProject.NeuroNet
+{
+    class InputLayer
+    {
+        private Random random = new Random();
+
+        private double[,] trainset = new double[100, 16]; //массив обучающей выборки
+        private double[,] testset = new double[10, 16]; // тестовой выборки
+
+        public double[,] Trainset { get => trainset; }
+        public double[,] Testset { get => testset; }
+
+        public InputLayer(NetworkMode nm)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string[] tmpStr;
+            string[] tmpArrStr;
+            double[] tmpArr;
+            switch (nm)
+            {
+                case NetworkMode.Train: // tak je dlya test
+                    tmpArrStr = File.ReadAllLines(path + "train.txt");
+                    for (int i = 0; i < tmpArrStr.Length; i++)
+                    {
+                        tmpStr = tmpArrStr[i].Split();
+                        tmpArr = new double[tmpStr.Length];
+                        for( int j = 0; j < tmpArrStr.Length; j++)
+                        {
+                            tmpArr[j] = double.Parse(tmpStr[j], System.Globalization.CultureInfo.InvariantCulture);
+
+                        }
+                    }
+                    for (int n = trainset.GetLength(0) - 1; n >= 1; n--)
+                    {
+                        int j = random.Next(n + 1);
+                        double[] temp = new double[trainset.GetLength(1)];
+                        for(int i = 0; i < trainset.GetLength(1); i++)
+                        {
+                            temp[i] = trainset[n, i];
+                        }
+                        for (int i = 0; i < trainset.GetLength(1); i++)
+                        {
+                            trainset[n, i] = trainset[j, i];
+                            trainset[j, i] = temp[i];
+                        }
+                    }
+                    break;
+                    //
+                    //дописать для кнопки тест
+
+            }
+        }
+        //перетасовка строк массива методом Фишера-Йетса
+        public void Shuffling_Array_Roms(double[,] arr)
+        {
+            int j; // номер случайно выбранной строки
+            Random random = new Random();
+            double[] temp = new double[arr.GetLength(1)]; //вспомогательный массив
+            for(int n = arr.GetLength(0) - 1; n >= 1; n--) // цикл перебора строк снизу вверх
+            {
+                j = random.Next(n + 1); //выбор случайной строки из выше расположенных строк
+                for(int i = 0; i < arr.GetLength(1); i++) //цикл копирования н-ой строки
+                {
+                    temp[i] = arr[n, i];
+                }
+                for(int i = 0; i < arr.GetLength(1); i++) // перестановки двух строк
+                {
+                    arr[n, i] = arr[j, i]; 
+                    arr[j, i] = temp[i];
+                }
+            }
+        }
+    }
+}
---
+++ OneProject/OneProject/NeuroNet/NetWork.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace OneProject.NeuroNet
+{
+    class NetWork
+    {
+        private InputLayer input_layer = null;
+        private HiddenLayer hidden_layer1 = new HiddenLayer(70, 15, NeuronType.Hidden, nameof(hidden_layer1));
+        private HiddenLayer hidden_layer2 = new HiddenLayer(30, 70, NeuronType.Hidden, nameof(hidden_layer2));
+        private OutputLayer output_layer = new OutputLayer(10, 30, NeuronType.Output, nameof(output_layer));
+
+        public double[] fact = new double[10];
+
+        private double[] e_error_avr;
+
+        public double[] E_error_avr { get => e_error_avr; set => e_error_avr = value; }
+
+        public NetWork() { }
+
+        public void ForwardPass( NetWork net, double[] netInputs)
+        {
+            net.hidden_layer1.Data = netInputs;
+            net.hidden_layer1.Recognize(null, net.hidden_layer2);
+            net.hidden_layer2.Recognize(null, net.output_layer);
+            net.output_layer.Recognize(net, null);
+        }
+        public void Train(NetWork net)
+        {
+            int epoches = 70; // кол-во эпох обучения
+            net.input_layer = new InputLayer(NetworkMode.Train); //инициализация входного слоя
+            double tmpSumError; // временная переменная суммы ошибок
+            double[] errors; // массив входного сигнала ошибки выходного
+            double[] temp_gsums1; // сумма локальных градиентов для 1 скрытого слоя
+            double[] temp_gsums2; // сумма локальных градиентов для 2 скрытого слоя
+
+            e_error_avr = new double[epoches];
+            for(int k = 0; k < epoches; k++)
+            {
+                e_error_avr[k] = 0;
+                net.input_layer.Shuffling_Array_Roms(net.input_layer.Trainset);
+                for(int i = 0; i < net.input_layer.Trainset.GetLength(0); i++)
+                {
+                    double[] tmpTrain = new double[15];
+                    for(int j = 0;j < tmpTrain.Length; j++)
+                    {
+                        tmpTrain[j] = net.input_layer.Trainset[i, j + 1];
+                    }
+                    ForwardPass(net, tmpTrain);
+
+                    tmpSumError = 0;
+                    errors = new double[net.fact.Length];
+                    for(int x = 0; x < errors.Length; x++)
+                    {
+                        if (x == net.input_layer.Trainset[i, 0])
+                            errors[x] = 1.0 - net.fact[x];
+                        else
+                            errors[x] = -net.fact[x];
+                        tmpSumError += errors[x] * errors[x] / 2;
+                    }
+                    e_error_avr[k] += tmpSumError / errors.Length;
+
+                    temp_gsums2 = net.output_layer.BackwardPass(errors);
+                    temp_gsums1 = net.hidden_layer2.BackwardPass(temp_gsums2);
+                    net.hidden_layer1.BackwardPass(temp_gsums1);
+                }
+                e_error_avr[k] /= net.input_layer.Trainset.GetLength(0);
+            }
+            net.input_layer = null;
+
+            net.hidden_layer1.WeightInitialize(MemoryMode.SET, nameof(hidden_layer1) + "_memory.csv");
+        }
+
+    }
+}
